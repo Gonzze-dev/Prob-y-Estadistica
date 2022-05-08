ac4ba61cde029e0331fbb4e2299f8e0bde38e1ee
Gonzze <errandoneagonzalo18@gmail.com>
2022-05-08 20:28:36 -0300

    cambio sin sentido

--- contador_numeros/contador.py
+++ contador_numeros/contador.py
@@ -2,10 +2,10 @@ from array import array
 from cgitb import text
 from cmath import log
 from PyEstadistica import prob_estadistica
-from openpyxl import Workbook
 
-# book = Workbook()
-# sheet = book.active
+
+book = Workbook()
+sheet = book.active
 
 texto = "10,5 20,2 15,4 11,6 13,8 20,5 14,0 16,5 17,0 12,0 15,4 21,3 22,0 23,1 15,9 16,5 13,0 18,0 16,0 17,3 17,8 19,0 13,4 17,3 14,2 15,0 16,2 19,0 15,5 16,0 "
 #contenido = input('insertar numeros: ')
@@ -49,3 +49,4 @@ tabla = prob_estadistica.get_tabla_intervalos(l_numeros, amplitud)
 
 for fila in tabla:
     print(fila)
+
