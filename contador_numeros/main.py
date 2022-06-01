669bfb79edfed3244c2ea656ffd3b7dbf2d76d33
Gonzalo-PC <errandoneagonzalo18@gmail.com>
2022-06-01 12:48:15 -0300

    contador de numeros modificado

--- contador_numeros/main.py
+++ contador_numeros/main.py
@@ -1,21 +1,13 @@
+from cgitb import text
 from openpyxl import Workbook
-from PyEstadistica import prob_estadistica
+from PyEstadistica import prob_estadistica as pe
+import utili_excel as uex
+import utili_lista as ulista
 
 book = Workbook()
 sheet = book.active
 
-lista = ("16.500 – 10.050 – 12.320 – 10.000 – 22.540 – 7.325 – 13.800 – 14.600 –"
-+ " 25.000 – 17.085 – 19.000 – 11.900 – 13.760 – 15.075 – 20.210 – 7.280 –"
-+ " 21.200 – 23.090 – 24.500 – 15.800 – 5.000 – 13.050 – 21.600 – 17.700 ")
+texto = ("4,5 ; 10 ; 6 ; 10 ; 6,5 ; 8 ; 11 ; 11,5 ; 10,5 ; 8 ; 7 ; 9,5 ; 11,5 ; 9 ; 5 ; 10 ; 5 ; 10,5 ; 6,5 ; 3,5 ; 10 ; 10 ; 6 ; 10,5 ; 9. 2 ")
 
-l_numeros = prob_estadistica.crear_lista_numeros(lista)
-l_numeros.sort()
-
-print('rango: ', prob_estadistica.get_rango(l_numeros[1], l_numeros[0]) )
-print('cant intervalos: ', prob_estadistica.get_cant_intervalos(len(l_numeros)) )
-
-for i in range(len(l_numeros)):
-    celda = "A" + str(i+1)
-    sheet[celda] = l_numeros[i]
-
-book.save('negocio.xlsx')
+uex.crear_ordenar_guardar(book, sheet, texto, "negocio")
+ulista.hacer_todo(texto)
